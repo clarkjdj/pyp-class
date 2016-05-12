68ba98589caefd6db1ccd609dc201e606cf59cff
Martin Zugnoni <martin.zugnoni@gmail.com>
2016-05-12 17:14:26 -0300

    Use str instead of basestring

--- unit-4-control-flow-structures/lesson-2-param-type/solutions/what_is_this.py
+++ unit-4-control-flow-structures/lesson-2-param-type/solutions/what_is_this.py
@@ -3,7 +3,7 @@ def what_is_this(param):
         return "This is a Boolean."
     elif isinstance(param, int):
         return "This is an Integer."
-    elif isinstance(param, basestring):
+    elif isinstance(param, str):
         return "This is a String."
     elif isinstance(param, float):
         return "This is a Float."
